9a229408b0a51c15c6b99f029eec2e6c65751aed
parth-root <poproot4@gmail.com>
2023-11-28 04:45:27 +0000

    gelexy book 3 spker fix

--- init.d/telegrame.sh
+++ init.d/telegrame.sh
@@ -7,10 +7,12 @@ telegram-send "lights out!! now ONLINE"
 fi
 while [ True ]
 do
-if [ $(cat /home/ghost/init.d/chack) == $(curl ifconfig.me) ];then echo "not sending"
+curl ipinfo.io/ip
+#if [ $(cat /home/ghost/init.d/chack) == $(curl ifconfig.me) ];then echo "not sending"
+if [ $(cat /home/ghost/init.d/chack) == $(curl ipinfo.io/ip) ];then echo "not sending"
 sleep 60
 else 
-curl ifconfig.me > /home/ghost/init.d/chack
+curl ipinfo.io/ip > /home/ghost/init.d/chack
 telegram-send "ip-change"
 telegram-send $(cat /home/ghost/init.d/chack)
 echo "send done"
